d9c1ea71cd1ec5730bdbf2711198cf8e4c8c3242
Vadym Berezdetskyi <berezdetskyi.v@paymaxi.com>
2022-03-08 15:08:11 +0200

    Fix grafana

--- grafana/setup.sh
+++ grafana/setup.sh
@@ -1,76 +1,78 @@
 #!/bin/bash
 
-# https://github.com/grafana/grafana-docker/issues/74
 
-GRAFANA_URL=${GRAFANA_URL:-http://$GF_SECURITY_ADMIN_USER:$GF_SECURITY_ADMIN_PASSWORD@localhost:3000}
-DATASOURCES_PATH=${DATASOURCES_PATH:-/etc/grafana/datasources}
-DASHBOARDS_PATH=${DASHBOARDS_PATH:-/etc/grafana/dashboards}
 
-# Generic function to call the Vault API
-grafana_api() {
-  local verb=$1
-  local url=$2
-  local params=$3
-  local bodyfile=$4
-  local response
-  local cmd
+# # https://github.com/grafana/grafana-docker/issues/74
 
-  cmd="curl -L -s --fail -H \"Accept: application/json\" -H \"Content-Type: application/json\" -X ${verb} -k ${GRAFANA_URL}${url}"
-  [[ -n "${params}" ]] && cmd="${cmd} -d \"${params}\""
-  [[ -n "${bodyfile}" ]] && cmd="${cmd} --data @${bodyfile}"
-  echo "Running ${cmd}"
-  eval ${cmd} || return 1
-  return 0
-}
+# GRAFANA_URL=${GRAFANA_URL:-http://$GF_SECURITY_ADMIN_USER:$GF_SECURITY_ADMIN_PASSWORD@localhost:3000}
+# DATASOURCES_PATH=${DATASOURCES_PATH:-/etc/grafana/datasources}
+# DASHBOARDS_PATH=${DASHBOARDS_PATH:-/etc/grafana/dashboards}
 
-wait_for_api() {
-  while ! grafana_api GET /api/user/preferences
-  do
-    sleep 5
-  done 
-}
+# # Generic function to call the Vault API
+# grafana_api() {
+#   local verb=$1
+#   local url=$2
+#   local params=$3
+#   local bodyfile=$4
+#   local response
+#   local cmd
 
-install_datasources() {
-  local datasource
+#   cmd="curl -L -s --fail -H \"Accept: application/json\" -H \"Content-Type: application/json\" -X ${verb} -k ${GRAFANA_URL}${url}"
+#   [[ -n "${params}" ]] && cmd="${cmd} -d \"${params}\""
+#   [[ -n "${bodyfile}" ]] && cmd="${cmd} --data @${bodyfile}"
+#   echo "Running ${cmd}"
+#   eval ${cmd} || return 1
+#   return 0
+# }
 
-  for datasource in ${DATASOURCES_PATH}/*.json
-  do
-    if [[ -f "${datasource}" ]]; then
-      echo "Installing datasource ${datasource}"
-      if grafana_api POST /api/datasources "" "${datasource}"; then
-        echo "installed ok"
-      else
-        echo "install failed"
-      fi
-    fi
-  done
-}
+# wait_for_api() {
+#   while ! grafana_api GET /api/user/preferences
+#   do
+#     sleep 5
+#   done 
+# }
 
-install_dashboards() {
-  local dashboard
+# install_datasources() {
+#   local datasource
 
-  for dashboard in ${DASHBOARDS_PATH}/*.json
-  do
-    if [[ -f "${dashboard}" ]]; then
-      echo "Installing dashboard ${dashboard}"
+#   for datasource in ${DATASOURCES_PATH}/*.json
+#   do
+#     if [[ -f "${datasource}" ]]; then
+#       echo "Installing datasource ${datasource}"
+#       if grafana_api POST /api/datasources "" "${datasource}"; then
+#         echo "installed ok"
+#       else
+#         echo "install failed"
+#       fi
+#     fi
+#   done
+# }
 
-      if grafana_api POST /api/dashboards/db "" "${dashboard}"; then
-        echo "installed ok"
-      else
-        echo "install failed"
-      fi
+# install_dashboards() {
+#   local dashboard
 
-    fi
-  done
-}
+#   for dashboard in ${DASHBOARDS_PATH}/*.json
+#   do
+#     if [[ -f "${dashboard}" ]]; then
+#       echo "Installing dashboard ${dashboard}"
 
-configure_grafana() {
-  wait_for_api
-  install_datasources
-  install_dashboards
-}
+#       if grafana_api POST /api/dashboards/db "" "${dashboard}"; then
+#         echo "installed ok"
+#       else
+#         echo "install failed"
+#       fi
 
-echo "Running configure_grafana in the background..."
-configure_grafana &
-/run.sh
-exit 0
+#     fi
+#   done
+# }
+
+# configure_grafana() {
+#   wait_for_api
+#   install_datasources
+#   install_dashboards
+# }
+
+# echo "Running configure_grafana in the background..."
+# configure_grafana &
+# /run.sh
+# exit 0
