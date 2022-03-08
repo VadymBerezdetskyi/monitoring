68453d560e22c7dcb814405a53a33b9d00d50b41
Vadym Berezdetskyi <berezdetskyi.v@paymaxi.com>
2022-03-08 14:17:08 +0200

    Fix alertmanager

--- alertmanager/conf/docker-entrypoint.sh
+++ alertmanager/conf/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-cat /etc/alertmanager/alertmanager.yml | sed -e "s,\${slack_api_url},${SLACK_URL}/" -e "s,\${channel},${SLACK_CHANNEL}/" > /tmp/alertmanager.yml
+cat /etc/alertmanager/alertmanager.yml | sed -e "s,\${slack_api_url},${SLACK_URL}," -e "s,\${channel},${SLACK_CHANNEL}," > /tmp/alertmanager.yml
 
 mv /tmp/alertmanager.yml /etc/alertmanager/alertmanager.yml
 
